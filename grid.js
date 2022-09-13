33b161b704c9d8aa1997051a792858ac5833d063
Khaled Alkhatib <khaledkhatib91@hotmail.com>
2022-09-13 19:41:27 -0400

    Change size of y grid

--- grid.js
+++ grid.js
@@ -3,12 +3,12 @@ export function randomGridPosition() {
     /* Our grid size is 29 and we add 1 because we agreed that everything starts from 1 not 0
     so by adding 1 the random number will be between 1 and 29  */
     x: Math.floor(Math.random() * 29) + 1,
-    y: Math.floor(Math.random() * 29) + 1
+    y: Math.floor(Math.random() * 40) + 1
   };
 }
 
 export function outsideGrid(position) {
   return (
-    position.x < 1 || position.x > 29 || position.y < 1 || position.y > 29
+    position.x < 1 || position.x > 29 || position.y < 1 || position.y > 40
   );
 }
