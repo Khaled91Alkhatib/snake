4df95a6424e3984f80fec7f9707f89c14f3257e2
Khaled Alkhatib <khaledkhatib91@hotmail.com>
2022-09-13 20:30:01 -0400

    Fix grid size bug

--- grid.js
+++ grid.js
@@ -2,13 +2,13 @@ export function randomGridPosition() {
   return {
     /* Our grid size is 29 and we add 1 because we agreed that everything starts from 1 not 0
     so by adding 1 the random number will be between 1 and 29  */
-    x: Math.floor(Math.random() * 29) + 1,
-    y: Math.floor(Math.random() * 40) + 1
+    x: Math.floor(Math.random() * 40) + 1,
+    y: Math.floor(Math.random() * 29) + 1
   };
 }
 
 export function outsideGrid(position) {
   return (
-    position.x < 1 || position.x > 29 || position.y < 1 || position.y > 40
+    position.x < 1 || position.x > 40 || position.y < 1 || position.y > 29
   );
 }
